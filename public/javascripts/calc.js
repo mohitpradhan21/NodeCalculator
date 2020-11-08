23a6d519de825c2d3c25466db7070691e6c33657
unknown <mohit_201911506@smit.smu.edu.in>
2020-11-08 17:05:39 +0530

    first commit

---
+++ public/javascripts/calc.js
@@ -0,0 +1,9 @@
+function backspace(calc) {
+    size = calc.display.value.length;
+    calc.display.value = calc.display.value.substring(0, size-1);
+}
+
+function calculate(calc) {
+    
+        calc.display.value = eval(calc.display.value);
+}
